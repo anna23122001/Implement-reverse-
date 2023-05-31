02bf5a9887fc8ad1f32ba96904c1ff9371f1e005
anna <anna.riazantseva123321@gmail.com>
2023-05-31 12:33:19 +0300

    the first version

---
+++ src/index.js
@@ -0,0 +1,21 @@
+'use strict'
+function MyArray(...args) {
+    this.length = 0;
+    for(let i = 0; i< args.length; i++){
+        this.push(args[i]);
+    }
+ }
+ MyArray.prototype = new MyArrayProto();
+ function MyArrayProto(){
+    this.reverse = function(fn){
+       for(let i = 0, j = this.length - 1; i < j; i++, j--){
+        [this[i], this[j]] = [this[j], this[i]];
+       }
+       return this
+    } 
+ }
+
+const myArray = [1, 2, 3, 4, 5];
+console.log(myArray);
+myArray.reverse();
+console.log(myArray);
